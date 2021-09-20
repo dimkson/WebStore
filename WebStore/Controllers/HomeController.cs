38d260a12b256a546dc7dc4e85fdce13c221f1a2
Dmitriy Bykov <dimkson@yahoo.com>
2021-09-20 14:11:20 +0300

    Добавлены страницы Блог и Корзина

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -5,5 +5,11 @@ namespace WebStore.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View();
+
+        public IActionResult Blog() => View();
+
+        public IActionResult BlogSingle() => View();
+        
+        public IActionResult Cart() => View();
     }
 }
