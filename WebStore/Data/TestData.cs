647e3e25084f0dd0e942634639c4e3dd6d555817
Dmitriy Bykov <dimkson@yahoo.com>
2021-09-20 12:59:48 +0300

    Добавлен контроллер Employees

---
+++ WebStore/Data/TestData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using WebStore.Models;
+
+namespace WebStore.Data
+{
+    public class TestData
+    {
+        public static List<Employee> Employees { get; } = new()
+        {
+            new Employee(1, "Иван", "Иванов", "Иванович", 30),
+            new Employee(2, "Петр", "Петров", "Петрович", 40),
+            new Employee(3, "Сидр", "Сидоров", "Сидорович", 25)
+        };
+    }
+}
